698bb618f12e2ee701f4d1ce9fbd1e59ba8f1a84
Mitchell Amundsen <mitchell.k.amundsen@gmail.com>
2016-03-23 02:33:03 -0700

    python start

---
+++ hard_way_python/ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun."
+print "Sike, I can think for myself"
+print "I'd much rather you 'not'"
+print "That last line was weird."
